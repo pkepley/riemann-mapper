354c52f70e043c1b9bf1a1787d5d244cd72ef800
Paul Kepley <pakepley@gmail.com>
2017-09-21 20:10:56 -0500

    Mostly(?) corrected. I am using arc-length instead of segment length in the 'integrals'

--- KernelsKS.py
+++ KernelsKS.py
@@ -14,10 +14,6 @@ class KernelsKS:
 		self.set_eval_points()
 		self.set_cauchy_ca()
 		self.set_KS_kernel()
-
-	def set_a(self, a):
-		self.a = a
-		self.set_cauchy_ca()
 		
 	def set_eval_points(self):
 		self.n_zs = self.points_on_gamma.n_ps
@@ -25,53 +21,50 @@ class KernelsKS:
 		self.z_prime_s = self.points_on_gamma.ps_tangent
 		self.Ts = self.points_on_gamma.ps_unit_tangent
 
+	def integral_test(self):
+		print sum(abs(self.points_on_gamma.ps_seg_len.flatten())) - self.points_on_gamma.gamma_length
+
 	def set_cauchy_ca(self):
 		# Cauchy Kernel, evaluated at z = ps and w = a
 		self.C_a = 	(1.0/(2.0*np.pi*1.0j)) * (
 			(self.Ts.conjugate()) / ((self.a - self.zs).conjugate()))
 
-		self.c = np.sqrt(abs(self.z_prime_s)) * self.C_a
-
 		# reshape
 		self.C_a = self.C_a.reshape((self.n_zs,1))
-		self.c = self.c.reshape((self.n_zs,1))
 		
 	def set_KS_kernel(self):
-		w = self.zs.reshape((1,self.n_zs))
-		Tw = self.Ts.reshape((1,self.n_zs))
-		abs_z_prime_s = abs(self.z_prime_s).reshape((1,self.n_zs))
+		w = self.zs.reshape((1, self.n_zs))
+		Tw = self.Ts.reshape((1, self.n_zs))
+		abs_z_prime_s = abs(self.z_prime_s).reshape((1, self.n_zs))
 
-		z = self.zs.reshape((self.n_zs,1))
-		Tz = self.Ts.reshape((self.n_zs,1))
-		abs_w_prime_s = abs(self.z_prime_s).reshape((self.n_zs,1))
+		z = self.zs.reshape((self.n_zs, 1))
+		Tz = self.Ts.reshape((self.n_zs, 1))
+		abs_w_prime_s = abs(self.z_prime_s).reshape((self.n_zs, 1))
 
 		# compute matrix for the KS kernel, A
 		A0 = np.zeros((self.n_zs, self.n_zs))
-		A0 = w - z		
+		A0 = w - z
+
 		np.fill_diagonal(A0,1) # don't divide by zero
 		A0 = 1.0 / A0
-		np.fill_diagonal(A0,0) # K will vanish along diagonal, so set this to 0
+		np.fill_diagonal(A0,0) # A will vanish along diagonal, so set this to 0
 
 		A1 = np.zeros((self.n_zs, self.n_zs))
-		A1 = np.dot(A0, np.diag(Tw.flatten()))
+ 		A1 = np.dot(A0, np.diag(Tw.flatten()))
 
 		A2 = np.zeros((self.n_zs, self.n_zs))
 		A2 = np.dot(np.diag(Tz.flatten()), A0)
 		A2 = A2.conjugate()
 
-		self.A = (1.0/(2.0*np.pi*1.0j)) *  (A1 - A2)	
+		self.A = (1.0/(2.0*np.pi*1.0j)) *  (A1 - A2)
 
-		self.K = np.dot(np.diag(abs_z_prime_s.flatten()),  self.A) 
-		self.K = np.dot(self.K, np.diag(abs_w_prime_s.flatten()))
 		
 	def solve_ks(self):
-		self.M = np.dot(self.K, np.diag(self.points_on_gamma.ps_seg_len.flatten()))
-		np.fill_diagonal(self.M, 1.0 - np.diag(self.M))
- 		self.sigma = np.linalg.solve(self.M, self.c)
+		delta_s = self.points_on_gamma.ps_seg_len.flatten()
+		self.M = np.eye(self.n_zs) - np.dot(self.A, np.diag(delta_s))
 
 		# Obtain the Szego kernel
-		self.S_a = self.sigma.flatten() / np.sqrt(abs(self.z_prime_s.flatten()))
-		self.S_a = self.S_a.reshape((self.n_zs, 1))
+		self.S_a = np.linalg.solve(self.M, self.C_a)
 
 		# Obtain the bv of the Garabedian Kernel
 		self.L_a = (1.0j) * (self.S_a.flatten() * self.Ts.flatten()).conjugate()
@@ -82,13 +75,15 @@ if __name__ == '__main__':
 	from svgpathtools import svg2paths, disvg, path
 	from PointsOnGamma import PointsOnGamma
 	import matplotlib.pyplot as plt
-	paths, attrs = svg2paths('./imgs/indiana_map.svg')
-	#paths, attrs = svg2paths('./imgs/circle2.svg')
+	# paths, attrs = svg2paths('./imgs/indiana_map.svg')
+	# paths, attrs = svg2paths('./imgs/circle2.svg')
+	# paths, attrs = svg2paths('./imgs/ellipse.svg')
+	paths, attrs = svg2paths('./imgs/square.svg')
 	debug_plots = True
 
 	# Distribute n points around a piece-wise continuous path
 	gamma = paths[0]
-	n = 200
+	n = 1000
 
 	pog = PointsOnGamma(gamma, n)
 	ps = pog.ps
@@ -96,23 +91,29 @@ if __name__ == '__main__':
 	ys = ps.imag
 
 	# not guaranteed to fall inside of gamma, but set a the mean of gamma
-	a = np.mean(xs) + np.mean(ys) * (1.0j)		
+	a = np.mean(ps)
 	kks = KernelsKS(a, pog)	
 	kks.solve_ks()
 
+	kks.integral_test()
+
 	if debug_plots:		
 		plt.figure()
-		plt.plot(abs(kks.c))
+		#plt.plot(abs(kks.c))
 		plt.plot(abs(kks.C_a))
 
 		plt.figure()
-		plt.plot(abs(kks.sigma))
+		#plt.plot(abs(kks.sigma))
 		plt.plot(abs(kks.S_a))
 
 		plt.figure()
 		plt.plot(abs(kks.L_a))
 				
 		plt.figure()
-		plt.imshow(abs(kks.A), interpolation='nearest')
+		plt.imshow(np.abs(kks.A), interpolation='nearest')
+		plt.colorbar()
+
+		plt.figure()
+		plt.imshow(abs(kks.A[0:50,0:50]), interpolation='nearest')
 		plt.colorbar()
 		plt.show()
