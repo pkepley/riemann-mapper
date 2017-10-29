aaf2809e21689b5c6ea833bf174613218d8d12fd
Paul Kepley <pakepley@gmail.com>
2017-10-29 16:11:36 -0500

    Use 5000 points and save the images

--- demos/indiana.py
+++ demos/indiana.py
@@ -8,13 +8,16 @@ from KernelsKS import KernelsKS
 from RiemannMapper import RiemannMapper
 import matplotlib.pyplot as plt
 
+save_image_flag = True
+
 # Indiana image
 paths, attrs = svg2paths('../imgs/indiana_map.svg')
 
-# Distribute n points around a piece-wise continuous path
-gamma = paths[0]
-n = 4000
+# Grab the path. Note that it's upside down, so we'll correct that first
+gamma = paths[0].rotated(180)
 
+# Distribute n points around a piece-wise continuous path,
+n = 5000
 pog = PointsOnGamma(gamma, n)
 zs_b = pog.ps
 xs_b = zs_b.real
@@ -38,10 +41,11 @@ i_xs = np.abs(winding_at_ps - 1) < 0.2
 ws = ps[i_xs]
 
 # Plot the path and grid
-plt.figure(1)
+plt.figure(1, figsize=(12,12))
 ws_xx, ws_yy = ws.real, ws.imag
 plt.plot(xs_b, ys_b)
 plt.scatter(ws_xx, ws_yy, marker='.')
+plt.axes().set_aspect('equal')
 
 # not guaranteed to fall inside of gamma, but set a the mean of gamma
 c = np.mean(ps)
@@ -56,10 +60,18 @@ rm = RiemannMapper(kks)
 zz = rm.evaluate_riemann(ws)
 
 x_zz, y_zz = zz[0].real, zz[0].imag
-plt.figure(4)
+plt.figure(2,figsize=(12,12))
 tt = np.linspace(0,2*np.pi,100)
 circ_xs, circ_ys = np.cos(tt), np.sin(tt)
 plt.plot(circ_xs, circ_ys, label="circle")
 plt.scatter(x_zz, y_zz, marker='.')
 plt.axis([-1, 1, -1, 1])
+plt.axes().set_aspect('equal')
+
+if save_image_flag:
+	plt.figure(1)
+	plt.savefig('../imgs/indianaSource.png', bbox_inches='tight')
+	plt.figure(2)
+	plt.savefig('../imgs/indianaTarget.png', bbox_inches='tight')
+
 plt.show()
