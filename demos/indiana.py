fe5d3e58996c760f27b34980b53ea0f108a11cab
Paul Kepley <pakepley@gmail.com>
2020-09-26 17:44:14 -0500

    Updated plot output

--- demos/indiana.py
+++ demos/indiana.py
@@ -37,16 +37,16 @@ ps = xs + (1.0j) * ys
 # Determine if a point is inside of Omega by checking if the
 # winding number is 1
 winding_at_ps = pog.cauchy_integral(np.ones(zs_b.shape), ps.T).reshape((nps, 1))
-print(np.abs(winding_at_ps))
 i_xs = np.abs(winding_at_ps - 1) < 0.2
 ws = ps[i_xs]
 
 # Plot the path and grid
-plt.figure(1, figsize=(12, 12))
+fig, ax = plt.subplots(figsize=(12, 12))
 ws_xx, ws_yy = ws.real, ws.imag
-plt.plot(xs_b, ys_b)
-plt.scatter(ws_xx, ws_yy, marker=".")
-plt.axes().set_aspect("equal")
+ax.plot(xs_b, ys_b)
+ax.scatter(ws_xx, ws_yy, c=ws_xx, marker=".", cmap="plasma")
+ax.set_axis_off()
+ax.set_aspect("equal")
 
 # not guaranteed to fall inside of gamma, but set a the mean of gamma
 c = np.mean(ps)
@@ -61,18 +61,19 @@ rm = RiemannMapper(kks)
 zz = rm.evaluate_riemann(ws)
 
 x_zz, y_zz = zz[0].real, zz[0].imag
-plt.figure(2, figsize=(12, 12))
+fig, ax = plt.subplots(figsize=(12, 12))
 tt = np.linspace(0, 2 * np.pi, 100)
 circ_xs, circ_ys = np.cos(tt), np.sin(tt)
-plt.plot(circ_xs, circ_ys, label="circle")
-plt.scatter(x_zz, y_zz, marker=".")
-plt.axis([-1, 1, -1, 1])
-plt.axes().set_aspect("equal")
+ax.plot(circ_xs, circ_ys, label="circle")
+ax.scatter(x_zz, y_zz, c=ws_xx, marker=".", cmap="plasma")
+ax.axis([-1, 1, -1, 1])
+ax.set_axis_off()
+ax.set_aspect("equal")
 
 if save_image_flag:
     plt.figure(1)
-    plt.savefig("../imgs/indianaSource.png", bbox_inches="tight")
+    plt.savefig("../imgs/indianaSource.png", bbox_inches="tight", pad_inches=0)
     plt.figure(2)
-    plt.savefig("../imgs/indianaTarget.png", bbox_inches="tight")
+    plt.savefig("../imgs/indianaTarget.png", bbox_inches="tight", pad_inches=0)
 
 plt.show()
